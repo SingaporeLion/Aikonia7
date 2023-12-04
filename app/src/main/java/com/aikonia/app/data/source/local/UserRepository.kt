a5321268d4079c441b0d0c899ac3022c15518fd5
SingaporeLion <144587396+SingaporeLion@users.noreply.github.com>
2023-12-04 10:28:40 +0100

    Update UserRepository.kt

--- app/src/main/java/com/aikonia/app/data/source/local/UserRepository.kt
+++ app/src/main/java/com/aikonia/app/data/source/local/UserRepository.kt
@@ -5,4 +5,5 @@ import com.aikonia.app.data.source.local.User
 interface UserRepository {
     suspend fun saveUser(user: User)
     suspend fun getUserById(userId: Int): User?
+    suspend fun getCurrentUserName(): String
 }
